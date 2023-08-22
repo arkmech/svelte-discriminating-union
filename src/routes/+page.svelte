5f8bcb24057b217799c14e21c0060b6fb18c20f1
arkmech <test@gmail.com>
2023-08-21 23:15:20 -0400

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+	import Test from './Test.svelte';
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<Test color="green" name="Hello" uniqueProp="Hi" />
+<Test color="red" name="Hello" uniqueProp="Hi" />
---
+++ src/routes/Test.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	type $$Props = {
+		name: string;
+	} & (
+		| { color?: 'red' }
+		| {
+				color: 'green';
+				uniqueProp?: string;
+		  }
+	);
+	export let name: $$Props['name'];
+	export let color: $$Props['color'];
+	export let uniqueProp: $$Props['uniqueProp'];
+</script>
+
+<div>{name}</div>
+{#if color === 'red'}
+	<div>Red</div>
+{:else if color === 'green'}
+	<div>Green</div>
+	<div>{uniqueProp}</div>
+{/if}
